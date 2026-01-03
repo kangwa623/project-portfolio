6d6d39ee507ec1f85a7baed6ddeab31844adc548
kangwa623 <kangwamuk3020@gmail.com>
2026-01-03 15:31:53 +0200

    added images to 'honours and awards' section

--- src/lib/constants/experience.ts
+++ src/lib/constants/experience.ts
@@ -182,28 +182,14 @@ export const EXPERIENCES: Experience[] = [
       {
         title: "Undergraduate Student Innovator of the Year Award",
         year: "Issued by University of Zambia • Nov 2025",
-        description: `
-- ♦ Developed **end-to-end features** across the stack using **Next.js,  Zustand, React Query, FastAPI and Mongodb**.
-- ♦ Conducted **code reviews**, mentored junior developers, and provided guidance on best practices.
-- ♦ Collaborated with **UI/UX designers** to translate **Figma designs** into **accessible, pixel-perfect, and responsive UIs**.
-- ♦ Worked closely with **DevOps engineers** to deploy backend infrastructure on **Kubernetes**.
-- ♦ Partnered with **AI engineers** to integrate **OpenAI models (text completion + embeddings)** and built a **RAG system using Elasticsearch**.
-            `,
+        description: "![UNZA Innovator of the Year](/og-images/experience/certificates-unza25.jpg)"
+        /*`
+- ♦ Implemented **backend functionalities**, designed **database models**, and optimized APIs using **FastAPI, Python, and MongoDB**.
+            `*/,
         skills: [
-          "Next.js",
-          "React",
-          "ShadCN UI",
-          "Zustand",
-          "React Query",
-          "Python",
-          "FastAPI",
-          "MongoDB",
-          "Docker",
-          "Redis",
-          "Kubernetes",
-          "S3",
-          "Elasticsearch",
-          "ARQ",
+          "Research Methods",
+          "Scientific Method",
+          "Innovation",
         ],
       },
       {
@@ -217,81 +203,56 @@ export const EXPERIENCES: Experience[] = [
 - ♦ Deployed backend services using **Docker & Docker Compose** and improved **CI/CD pipelines** with **GitHub Actions**.
             `,
         skills: [
-          "Python",
-          "FastAPI",
-          "MongoDB",
-          "Docker",
-          "Redis",
-          "ARQ",
-          "GoLang",
-          "Next.js",
-          "React",
+          "NLP",
+          "NLG",
+          "Machine Learning",
         ],
       },
       {
         title: "1st Place - University of Zambia Innovation Challenge",
         year: "Issued by University of Zambia - Directorate of Innovation, Technology Transfer and Commercialization | Zambia Research and Development Centre • Jul 2025",
-        description: `
+        description: "![ZRDC](/og-images/experience/certificates-zrdc25.jpg)"
+        /*`
 - ♦ Implemented **backend functionalities**, designed **database models**, and optimized APIs using **FastAPI, Python, and MongoDB**.
-- ♦ Developed and maintained a **background worker system** using **ARQ and Redis**.
-- ♦ Brainstormed and debugged **new features and functionalities** with the engineering team.
-- ♦ Refactored and cleaned code, implementing **better coding patterns** and enforcing **formatting & linting rules** using **Ruff**.
-- ♦ Deployed backend services using **Docker & Docker Compose** and improved **CI/CD pipelines** with **GitHub Actions**.
-            `,
+            `*/,
         skills: [
-          "Python",
-          "FastAPI",
+          "Llama-3 8B",
+          "NLP",
+          "NLG",
+          "React.js",
+          "TailwindCSS",
+          "Node.js",
+          "Express.js",
           "MongoDB",
-          "Docker",
-          "Redis",
-          "ARQ",
-          "GoLang",
-          "Next.js",
-          "React",
         ],
       },
       {
         title: "3rd Place - IEEE IAS IPCSD Engineering Design Contest Africa",
-        year: "Institute of Electrical and Electronic Engineers, Industrial Applications Society - Industrial Power Conversion Systems Department • Oct 2024",
-        description: `
+        year: "Institute of Electrical and Electronics Engineers, Industrial Applications Society - Industrial Power Conversion Systems Department • Oct 2024",
+        description: "![Engineering Design Contest Africa](/og-images/experience/certificates-ipcsd24.jpg)"
+        /*`
 - ♦ Implemented **backend functionalities**, designed **database models**, and optimized APIs using **FastAPI, Python, and MongoDB**.
-- ♦ Developed and maintained a **background worker system** using **ARQ and Redis**.
-- ♦ Brainstormed and debugged **new features and functionalities** with the engineering team.
-- ♦ Refactored and cleaned code, implementing **better coding patterns** and enforcing **formatting & linting rules** using **Ruff**.
-- ♦ Deployed backend services using **Docker & Docker Compose** and improved **CI/CD pipelines** with **GitHub Actions**.
-            `,
+            `*/,
         skills: [
-          "Python",
-          "FastAPI",
-          "MongoDB",
-          "Docker",
-          "Redis",
-          "ARQ",
-          "GoLang",
-          "Next.js",
-          "React",
+          "Arduino Uno",
+          "C++",
+          "Proteus",
+          "Embedded Systems",
+          "Django",
+          "ESP32",
         ],
       },
       {
         title: "3rd Place - Zambia@60 National ICT Hackathon",
         year: "Issued by Ministry of Education - Zambia | National Science Centre | ProBase Group • Oct 2024",
-        description: `
+        description: "![Zambia@60](/og-images/experience/certificates-zambia@60_24.jpg)"
+        /*`
 - ♦ Implemented **backend functionalities**, designed **database models**, and optimized APIs using **FastAPI, Python, and MongoDB**.
-- ♦ Developed and maintained a **background worker system** using **ARQ and Redis**.
-- ♦ Brainstormed and debugged **new features and functionalities** with the engineering team.
-- ♦ Refactored and cleaned code, implementing **better coding patterns** and enforcing **formatting & linting rules** using **Ruff**.
-- ♦ Deployed backend services using **Docker & Docker Compose** and improved **CI/CD pipelines** with **GitHub Actions**.
-            `,
+            `*/,
         skills: [
           "Python",
-          "FastAPI",
-          "MongoDB",
-          "Docker",
-          "Redis",
-          "ARQ",
-          "GoLang",
-          "Next.js",
-          "React",
+          "TailwindCSS",
+          "Flutter",
         ],
       },
     ],
