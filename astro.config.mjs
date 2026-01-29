63fb6da91e4ca88af7fc67367e1687bdb97c659b
kangwa623 <kangwamuk3020@gmail.com>
2026-01-29 21:26:52 +0200

    migrate to kangwa-mukuka Firebase project

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kangwamukuka.web.app',
+  site: 'https://kangwa-mukuka.web.app',
   integrations: [react(), mdx()],
   vite: {
     plugins: [tailwindcss()],
