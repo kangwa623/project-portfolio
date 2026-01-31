01675796d912b35729b5fba5cff19e7d30b783ae
kangwa623 <kangwamuk3020@gmail.com>
2026-01-31 10:51:36 +0200

    adjusted DataLab tagline

--- src/lib/constants/experience.ts
+++ src/lib/constants/experience.ts
@@ -9,23 +9,18 @@ export const EXPERIENCES: Experience[] = [
         title: "Research Assistant - Enterprise Medical Imaging | DataLab Research Group",
         year: "Sep 2025 - Present",
         description: `
-- ♦ Lemme build something cool first... then I'll tell you what I did 😉.
+- ♦ Developing software solutions to enhance radiology workflows at the Cancer Diseases Hospital (CDH) in Zambia.
+- ♦ Researching on the use of Vision Language Models to semi-automate cervical cancer report generation at CDH.
+
             `,
         skills: [
-          "TypeScript",
-          "React",
-          "Zustand",
-          "Tailwind CSS",
-          "Node.js",
-          "Fastify",
-          "BullMQ",
-          "Redis",
-          "PostgreSQL",
-          "Docker",
-          "Playwright",
-          "Vite",
-          "Nx",
-          "Java",
+          "PyTorch",
+          "CUDA",
+          "NLP",
+          "NLG",
+          "AI",
+          "Machine Learning",
+          "Python",
         ]
       },
     ],
@@ -37,71 +32,50 @@ export const EXPERIENCES: Experience[] = [
         title: "Chairperson",
         year: "Dec 2024 - Nov 2025",
         description: `
-- ♦ Developed **end-to-end features** across the stack using **Next.js,  Zustand, React Query, FastAPI and Mongodb**.
-- ♦ Conducted **code reviews**, mentored junior developers, and provided guidance on best practices.
-- ♦ Collaborated with **UI/UX designers** to translate **Figma designs** into **accessible, pixel-perfect, and responsive UIs**.
-- ♦ Worked closely with **DevOps engineers** to deploy backend infrastructure on **Kubernetes**.
-- ♦ Partnered with **AI engineers** to integrate **OpenAI models (text completion + embeddings)** and built a **RAG system using Elasticsearch**.
+- ♦ Spearheaded and oversaw technical activities for students interested in STEM by facilitating educational talks,
+industrial trips, project proposals and leading the Student Branch Executive Team.
             `,
         skills: [
-          "Next.js",
-          "React",
-          "ShadCN UI",
-          "Zustand",
-          "React Query",
-          "Python",
-          "FastAPI",
-          "MongoDB",
-          "Docker",
-          "Redis",
-          "Kubernetes",
-          "S3",
-          "Elasticsearch",
-          "ARQ",
+          "Leadership",
+          "Team building",
+          "Strategy",
+          "Collaboration",
+          "Public Speaking",
+          "Community Management",
+          "Interpersonal skills",
         ],
       },
       {
         title: "Vice-Chairperson",
         year: "Dec 2023 - Dec 2024",
         description: `
-- ♦ Implemented **backend functionalities**, designed **database models**, and optimized APIs using **FastAPI, Python, and MongoDB**.
-- ♦ Developed and maintained a **background worker system** using **ARQ and Redis**.
-- ♦ Brainstormed and debugged **new features and functionalities** with the engineering team.
-- ♦ Refactored and cleaned code, implementing **better coding patterns** and enforcing **formatting & linting rules** using **Ruff**.
-- ♦ Deployed backend services using **Docker & Docker Compose** and improved **CI/CD pipelines** with **GitHub Actions**.
+- ♦ As the Junior Executive Officer, I helped the Branch Chairperson with the workload, throughout the semester.
             `,
         skills: [
-          "Python",
-          "FastAPI",
-          "MongoDB",
-          "Docker",
-          "Redis",
-          "ARQ",
-          "GoLang",
-          "Next.js",
-          "React",
+          "Leadership",
+          "Team building",
+          "Strategy",
+          "Collaboration",
+          "Public Speaking",
+          "Community Management",
+          "Interpersonal skills",
         ],
       },
       {
         title: "IEEEXtreme 17.0 Student Ambassador",
         year: "Apr 2023 - Dec 2023",
         description: `
-- ♦ Implemented **backend functionalities**, designed **database models**, and optimized APIs using **FastAPI, Python, and MongoDB**.
-- ♦ Developed and maintained a **background worker system** using **ARQ and Redis**.
-- ♦ Brainstormed and debugged **new features and functionalities** with the engineering team.
-- ♦ Refactored and cleaned code, implementing **better coding patterns** and enforcing **formatting & linting rules** using **Ruff**.
-- ♦ Deployed backend services using **Docker & Docker Compose** and improved **CI/CD pipelines** with **GitHub Actions**.
+- ♦ Oversaw the success of IEEEXtreme 17.0 at the University of Zambia Student Branch..
+- ♦ Scheduled weekly workshops and practice sessions based on competitive programming..
+- ♦ Conduct market research for strategic planning.
             `,
         skills: [
+          "Problem-solving",
           "Python",
-          "FastAPI",
-          "MongoDB",
-          "Docker",
-          "Redis",
-          "ARQ",
-          "GoLang",
-          "Next.js",
-          "React",
+          "Java",
+          "C++",
+          "Algorithms",
+          "Data Structures",
         ],
       },
     ],
@@ -141,11 +115,10 @@ export const EXPERIENCES: Experience[] = [
         title: "Technical Lead",
         year: "Sep 2023 - Jun 2024",
         description: `
-- ♦ Redesigned and developed a **conceptual landing page** for the company using **Next.js, React, and Tailwind CSS**.
-- ♦ Collaborated with **full-stack engineers**, gaining insights into **best practices** in a professional development environment.
-- ♦ Received feedback from **UI/UX and graphic designers** to refine and enhance the visual and user experience of my designs.
+- ♦ Guided the technical aspects of the club's projects and initiatives, ensuring their successful implementation at the
+University of Zambia. 
             `,
-        skills: ["Next.js", "React", "Tailwind CSS", "Figma", "Node.js"],
+        skills: ["Problem-solving", "Public speaking", "Interpersonal skills", "Team building", "Community engagement"],
       },
     ],
   },
@@ -157,21 +130,18 @@ export const EXPERIENCES: Experience[] = [
         title: "BSc. Computer Science (Major: Computer Systems Engineering) | University of Zambia (UNZA)",
         year: "Feb 2022 - May 2026",
         description: `
-- ♦ Built a strong foundation in **software engineering**, focusing on **web development** and **UI/UX principles**.
-- ♦ Gained hands-on experience with **C/C++ and Linux**, enhancing problem-solving and system-level programming skills.
+- ♦ Built a strong foundation in **computer science**, fundamentals. 
+- ♦ Gained hands-on experience with **C++ and Java**, enhancing problem-solving and system-level programming skills.
 - ♦ Developed expertise in **relational databases** and **networking**, applying concepts to real-world applications.
 - ♦ Strengthened **communication and teamwork skills** through group projects, presentations, and technical discussions.
             `,
         skills: [
-          "C/C++",
-          "Linux",
-          "UI/UX Foundation",
-          "Marketing",
-          "WordPress",
+          "C++",
+          "Java",
+          "Python",
           "Networking",
-          "Algorithms",
+          "Algorithms and Data Structures",
           "Relational Databases",
-          "Software Engineering",
           "Self-learning",
           "Teamwork",
           "Presentation",
